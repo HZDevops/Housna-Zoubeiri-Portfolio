97f458a7643b97bfb2d49a84f6dab0a089d5b4cd
Zoubeiri <housna.zoubeiri@gmail.com>
2022-03-10 21:57:14 +0100

    modifying port server

--- front-end/contact-form.js
+++ front-end/contact-form.js
@@ -1,3 +1,15 @@
+var contactModal = new bootstrap.Modal(document.getElementById('contactModal'), {
+  keyboard: false,
+});
+
+//Display a pop-up after sending a message
+function displayPopUp() {
+  contactModal.show();
+  contactModal.addEventListener('hide.bs.modal', function (e) {
+   window.location.href = 'index.html';
+  });
+}
+
 //Put shopping-cart form values in an object and send to server for getting order Id if shopping-cart not empty
 function sendContactFormToServer() {
   const form = document.getElementById('contact-form');
@@ -7,7 +19,7 @@ function sendContactFormToServer() {
   const contactEmailInput = document.getElementById('contact-email');
   const contactMessageInput = document.getElementById('contact-message');
 
-  if (
+  /*if (
     contactFirstNameInput &&
     contactNameInput &&
     contactEmailInput &&
@@ -16,34 +28,35 @@ function sendContactFormToServer() {
     contactFormSendButton.disabled = true;
   } else {
     contactFormSendButton.disabled = false;
-  
-    form.addEventListener('submit', function (e) {
-      e.preventDefault();
-      const contact = {
-        firstName: document.getElementById('contact-first-name').value,
-        lastName: document.getElementById('contact-name').value,
-        email: document.getElementById('contact-email').value,
-        message: document.getElementById('contact-message').value,
-      };
-        console.log(contact)
-      // Post contact information to server 
-        fetch('http://localhost:3000/api/contacts', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(contact),
-        })
-          .then(function (response) {
-            if (response.ok) {
-                console.log(response)
-              return response.json();
-            }
-          })
-          .catch(function (error) {
-            document.getElementById('error-message').innerText =
-              'Serveur momentanémment indisponible';
-          });
-    });
-  }
+  }*/
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const contact = {
+      firstname: document.getElementById('contact-first-name').value,
+      name: document.getElementById('contact-name').value,
+      email: document.getElementById('contact-email').value,
+      message: document.getElementById('contact-message').value,
+    };
+
+    // Post contact information to server
+    fetch('http://localhost:3000/api/contacts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(contact),
+    })
+      .then(function (response) {
+        if (response.ok) {
+          contactModal.show();
+          window.location.href = 'index.html';
+          return response.json();
+        }
+      })
+      .catch(function (error) {
+        document.getElementById('error-message').innerText =
+          'Serveur momentanémment indisponible';
+      });
+  });
 }
+sendContactFormToServer();
